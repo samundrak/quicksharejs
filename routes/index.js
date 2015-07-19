303bfe631626753bf939dc62a633d24aae4156ae
Samundra Kc <Samundrak@yahoo.com>
2015-07-19 22:27:16 +0545

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -2,17 +2,14 @@ var express = require('express');
 var router = express.Router();
 var quickShare = require('./QuickShare');
 quickShare.setSharePath("/");
-quickShare.listPath = "/file";
+quickShare.listPath = "/";
 quickShare.downloadPath = "/:downloads";
 
 
 router.get(quickShare.listPath,quickShare.getFileList);
 router.get(quickShare.listPath + quickShare.downloadPath ,quickShare.getFile);
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+ 
 
 
 module.exports = router;
